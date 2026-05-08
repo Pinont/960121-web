e9fa82b83f1f028b4687cf30563ea0b1535f840d
Nonnipat Tangrojjanakhajorn <pinozen.thailand@gmail.com>
2026-05-08 10:49:35 +0700

    fix: fix error path

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ const PORT = process.env.PORT || 3000;
 // Global database connection
 let globalDb = null;
 
-// Middleware to attach database to request
-app.use((req, res, next) => {
-  req.db = globalDb;
-  next();
-});
-
 // Middleware
 app.use(cors({
   origin: '*',
@@ -35,6 +29,11 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.db = globalDb;
+  next();
+});
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
